4454bcbe1af0f29e53a29719158cc33424b8ab19
Usmon <usmon1983@gmail.com>
2020-10-03 18:44:47 +0500

    initialized go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/usmon1983/wallet
+
+go 1.15
---
+++ pkg/types/types.go
@@ -0,0 +1,29 @@
+package types
+
+type Money int64
+
+type PaymentCategory string
+
+type PaymentStatus string
+
+const (
+	PaymentStatusOk PaymentStatus = "OK"
+	PaymentStatusFail PaymentStatus = "FAIL"
+	PaymentStatusInProgress PaymentStatus = "INPROGRESS"
+)
+
+type Payment struct {
+	ID string
+	AccountID int64
+	Amount Money
+	Category PaymentCategory
+	Status PaymentStatus
+}
+
+type Phone string
+
+type Account struct {
+	ID int64
+	Phone Phone
+	Balance Money
+}
